0ff01b63652b6ed1229d8c99ee6f1b979b6469e1
Indra Lukmana <indralukmana@live.com>
2019-10-27 21:00:42 +0700

    ✨ add async fontloading on app load

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { AppLoading } from 'expo'
+
+import * as Font from 'expo-font'
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  const fontLoad = () =>
+    Font.loadAsync({
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    })
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fontLoad}
+        onFinish={() => setFontLoaded(true)}
+        onError={error => console.log(error)}
+      />
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +38,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
