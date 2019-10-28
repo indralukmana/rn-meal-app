4cb9d9ccc64693c270e74189404ee64816992e7d
Indra Lukmana <indralukmana@live.com>
2019-10-28 09:04:06 +0700

    ✨ add new default header styling through react-navigation

--- navigation/MealsNavigator.tsx
+++ navigation/MealsNavigator.tsx
@@ -1,13 +1,30 @@
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { Platform } from 'react-native'
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealScreen from '../screens/CategoryMealScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
+import Colors from '../constants/Colors'
 
-const MealNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: CategoryMealScreen,
-  MealDetail: MealDetailScreen,
-})
+const MealNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+      navigationOptions: { headerTitle: 'Meal Categories' },
+    },
+    CategoryMeals: CategoryMealScreen,
+    MealDetail: MealDetailScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerTitle: 'Meal App',
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+      },
+      headerTintColor:
+        Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+    },
+  }
+)
 
 export default createAppContainer(MealNavigator)
