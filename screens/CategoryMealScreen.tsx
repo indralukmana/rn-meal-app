abd0a5c1e13933ef5b97d764c05931e1b8e3f0cc
Indra Lukmana <indralukmana@live.com>
2019-10-28 12:03:03 +0700

    ✨ add meal list item component and style

--- screens/CategoryMealScreen.tsx
+++ screens/CategoryMealScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, StyleSheet, Button, FlatList } from 'react-native'
 import { CATEGORIES, MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 const CategoryMealScreen = props => {
   const categoryId = props.navigation.getParam('categoryId')
@@ -11,9 +12,14 @@ const CategoryMealScreen = props => {
 
   const renderMealItem = itemData => {
     return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
+      <MealItem
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        image={itemData.item.imageUrl}
+        onSelectMeal={() => {}}
+      />
     )
   }
 
@@ -23,6 +29,7 @@ const CategoryMealScreen = props => {
         data={displayedMeals}
         keyExtractor={item => item.id}
         renderItem={renderMealItem}
+        style={{ width: '100%' }}
       />
     </View>
   )
