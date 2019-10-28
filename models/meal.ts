ede7529995e19ea074f942e8b6639eaca9a1732b
Indra Lukmana <indralukmana@live.com>
2019-10-28 10:50:07 +0700

    ✨ add meals dummy data

---
+++ models/meal.ts
@@ -0,0 +1,47 @@
+class Meal {
+  id: any
+  categoryIds: any
+  title: any
+  affordability: any
+  complexity: any
+  imageUrl: any
+  duration: any
+  ingredients: any
+  steps: any
+  isGlutenFree: any
+  isVegan: any
+  isVegetarian: any
+  isLactoseFree: any
+
+  constructor(
+    id,
+    categoryIds,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    ingredients,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree
+  ) {
+    this.id = id
+    this.categoryIds = categoryIds
+    this.title = title
+    this.affordability = affordability
+    this.complexity = complexity
+    this.imageUrl = imageUrl
+    this.duration = duration
+    this.ingredients = ingredients
+    this.steps = steps
+    this.isGlutenFree = isGlutenFree
+    this.isVegan = isVegan
+    this.isVegetarian = isVegetarian
+    this.isLactoseFree = isLactoseFree
+  }
+}
+
+export default Meal
