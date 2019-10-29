0b6d73681c3b9ec3c73cec613c96d63c493411bb
Indra Lukmana <indralukmana@live.com>
2019-10-29 07:39:25 +0700

    ✨ add filter actions

--- store/reducers/meals.ts
+++ store/reducers/meals.ts
@@ -1,5 +1,5 @@
 import { MEALS } from '../../data/dummy-data'
-import { TOGGLE_FAVORITE } from '../actions/meals'
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/meals'
 
 const initialState = {
   meals: MEALS,
@@ -26,10 +26,30 @@ const mealsReducer = (state = initialState, action) => {
         console.log({ meal })
         return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) }
       }
+
+    case SET_FILTERS:
+      const appliedFilters = action.filters
+      const updatedFilteredMeals = state.meals.filter(meal => {
+        if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+          return false
+        }
+        if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+          return false
+        }
+        if (appliedFilters.vegetarian && !meal.isVegetarian) {
+          return false
+        }
+        if (appliedFilters.vegan && !meal.isVegan) {
+          return false
+        }
+        return true
+      })
+
+      return { ...state, filteredMeals: updatedFilteredMeals }
+
     default:
       return state
   }
-  return state
 }
 
 export default mealsReducer
